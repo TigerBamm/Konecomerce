0855decca4bb1890f5f7940544f17a39e6c19bb0
TigerBamm <mengkorngsreang025@gmail.com>
2026-03-05 12:52:03 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-   <h1 className="text-4xl text-red-500 font-bold">
-  Tailwind Working!
-</h1>
-  )
+    <div>
+      <Navbar />
+      <h1 className="text-4xl text-red-500 font-bold">Tailwind Working!</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import NavLink from './NavLink'
+
 
 function Navbar() {
   return (
     <div>
       <nav>
         <ul>
-            <li></li>
+            <li><NavLink to="/home">Home</NavLink></li>
+            <li><NavLink to="/products">Products</NavLink></li>
+            <li><NavLink to="/about">About</NavLink></li>
         </ul>
       </nav>
     </div>
